cdfa38c3d549335ad63ef9c68bb7268a5fe0c465
hoodedice <hoodedice@gmail.com>
2021-05-19 18:05:21 +0400

    Add store and dropdown for band select

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,11 +2,23 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 
-const NotFoundPage = () => (
-  <Layout>
+const NotFoundPage = () => {
+  const [isMount, setMount] = React.useState(false);
+  
+  React.useEffect(() => {
+        setMount(true);
+  },[])
+
+  if(!isMount) {
+     return(
+        <div>loading</div>
+     )
+  }
+
+  return (<Layout>
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+  </Layout>)
 
+}
 export default NotFoundPage
